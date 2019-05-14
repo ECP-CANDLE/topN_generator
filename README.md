28b12db62810eba4e4b5b0cf95fe042b8a0edb56
Harry <hsyoo@anl.gov>
2019-05-14 13:25:04 -0500

    init. download data files

---
+++ README.md
@@ -0,0 +1,1 @@
+# topN_generator
---
+++ download.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate -P data -nc ftp://ftp.mcs.anl.gov/pub/candle/public/benchmarks/Pilot1/combo/combined_rnaseq_data_lincs1000_combat
+wget --no-check-certificate -P data -nc ftp://ftp.mcs.anl.gov/pub/candle/public/benchmarks/Pilot1/combo/combined_single_response_agg 
+# wget --no-check-certificate ftp://ftp.mcs.anl.gov/pub/candle/public/benchmarks/Pilot1/combo/
